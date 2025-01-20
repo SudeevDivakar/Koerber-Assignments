b3cc87ce4a7ede34e6c09d8c7419c408a46d75ef
SUDEEV DIVAKAR <sudeev.divakar@koerber-supplychain.com>
2025-01-20 15:24:49 +0530

    Finish Day 7

---
+++ src/Day7/Question2/Author.java
@@ -0,0 +1,16 @@
+package Day7.Question2;
+import java.lang.annotation.*;
+
+/*
+Now write an program to makes use of the new API to read the Annotation related information that are applied on various Java
+Elements. Write an utility method called readAnnotation() takes a parameter of type AnnotationElement which can represent a
+Class, method or Constructor. Then it queries for a list of Annotations of that particular element by calling the getAnnotations()
+method. Then the array is iterated to get the individual element, then made a downcast to extract the exact information –
+Author.name() and Version.number().
+ */
+
+@Target(value = { ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Author {
+    String name() default "unknown";
+}
